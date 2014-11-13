6000857d7a1024ba2145a97b69fe703d57501893
hugo <hugo@soixantecircuits.fr>
2014-11-13 17:55:18 +0100

    simple reload of image

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,10 @@
 'use strict';
 
 $(function(){
-    var locations = ['views/about.html', 'views/main.html'];
+    var url = $('#image1').attr('src');
     var countUp = function () {
-        var pageurl = locations[Math.floor(Math.random() * locations.length)];
-        $.ajax({url:pageurl, success: function(data){
-            window.URL.revokeObjectURL($('#image1').attr('src'));
-            window.URL.revokeObjectURL($('#image2').attr('src'));
-            $('#content').html(data);
-            $('#image1').attr('src', $('#image1').attr('src') + '?time='+Date.now());
-            $('#image2').attr('src', $('#image2').attr('src') + '?time='+Date.now());
-
+            $('#image1').attr('src', url + '?time='+Date.now());
             setTimeout(countUp, 1500);
-        }});
     }
     setTimeout(countUp, 1500);
 });
