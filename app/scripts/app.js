9873bfafed0ef1d40c0dfc552f89d5643d266772
hugo <hugo@soixantecircuits.fr>
2014-11-13 17:47:42 +0100

    ajax load

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,18 @@
 'use strict';
-var locations = ['index.html', 'index2.html'];
-function countUp() {
-        window.location = locations[Math.floor(Math.random() * locations.length)];
-        setTimeout(countUp, 1500);
-    }
 
+$(function(){
+    var locations = ['views/about.html', 'views/main.html'];
+    var countUp = function () {
+        var pageurl = locations[Math.floor(Math.random() * locations.length)];
+        $.ajax({url:pageurl, success: function(data){
+            window.URL.revokeObjectURL($('#image1').attr('src'));
+            window.URL.revokeObjectURL($('#image2').attr('src'));
+            $('#content').html(data);
+            $('#image1').attr('src', $('#image1').attr('src') + '?time='+Date.now());
+            $('#image2').attr('src', $('#image2').attr('src') + '?time='+Date.now());
+
+            setTimeout(countUp, 1500);
+        }});
+    }
     setTimeout(countUp, 1500);
+});
